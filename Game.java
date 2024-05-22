60a32b8e79e6fa3824c7ba97e204f7d0d93f3610
Jayden Lin <168321799+NewYorkImperialist@users.noreply.github.com>
2024-05-22 09:34:37 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -9,7 +9,7 @@ public class Game {
     }
 
     public String format(int n) {
-      return String.format("%6s", n > 0 ? "" + n : "");
+        return String.format("%6s", n > 0 ? "" + n : "");
     }
 
     public void printArray() {
@@ -44,118 +44,48 @@ public class Game {
                 }
             }
         }
-            if (emptySpacesX.isEmpty()) {
-                return;
-            }
-            int choice = r.nextInt(emptySpacesX.size());
-            int numberChooser = r.nextInt(10); //value 0-9
-            int newNumber = 2;
-            if (numberChooser == 0) {
-                newNumber = 4;
-            }
-            int X = emptySpacesX.get(choice);
-            int Y = emptySpacesY.get(choice);
-            gameBoard[X][Y] = newNumber;
+        if (emptySpacesX.isEmpty()) {
+            return;
         }
+        int choice = r.nextInt(emptySpacesX.size());
+        int numberChooser = r.nextInt(10); //value 0-9
+        int newNumber = 2;
+        if (numberChooser == 0) {
+            newNumber = 4;
+        }
+        int X = emptySpacesX.get(choice);
+        int Y = emptySpacesY.get(choice);
+        gameBoard[X][Y] = newNumber;
+    }
 
-    public void pushUp() {
+    public void pushUp(int[][] board) {
         System.out.println("Pushing up...");
         for (int y = 0; y < 4; y++) {
             boolean[] alreadyCombined = {false, false, false, false};
             for (int x = 1; x < 4; x++) {
-                if (gameBoard[x][y] != 0) {
-                    int value = gameBoard[x][y];
+                if (board[x][y] != 0) {
+                    int value = board[x][y];
                     int X = x - 1;
-                    while ((X >= 0) && (gameBoard[X][y] == 0)) {
+                    while ((X >= 0) && (board[X][y] == 0)) {
                         X--;
                     }
                     if (X == -1) {
-                        gameBoard[0][y] = value;
-                        gameBoard[x][y] = 0;
-                    } else if (gameBoard[X][y] != value) {
+                        board[0][y] = value;
+                        board[x][y] = 0;
+                    } else if (board[X][y] != value) {
                         if (X + 1 != x) { // Check if shifting occurred
-                            gameBoard[X + 1][y] = value;
-                            gameBoard[x][y] = 0;
+                            board[X + 1][y] = value;
+                            board[x][y] = 0;
                         }
                     } else {
                         if (!alreadyCombined[X]) {
-                            gameBoard[X][y] *= 2;
-                            gameBoard[x][y] = 0;
+                            board[X][y] *= 2;
+                            board[x][y] = 0;
                             alreadyCombined[X] = true;
                         } else {
                             if (X + 1 != x) { // Check if shifting occurred
-                                gameBoard[X + 1][y] = value;
-                                gameBoard[x][y] = 0;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    public void pushDown() {
-        System.out.println("Pushing down...");
-        for (int y = 0; y < 4; y++) {
-            boolean[] alreadyCombined = {false, false, false, false};
-            for (int x = 2; x > -1; x--) {
-                if (gameBoard[x][y] != 0) {
-                    int value = gameBoard[x][y];
-                    int X = x + 1;
-                    while ((X <= 3) && (gameBoard[X][y] == 0)) {
-                        X++;
-                    }
-                    if (X == 4) {
-                        gameBoard[3][y] = value;
-                        gameBoard[x][y] = 0;
-                    } else if (gameBoard[X][y] != value) {
-                        if (X - 1 != x) { // Check if shifting occurred
-                            gameBoard[X - 1][y] = value;
-                            gameBoard[x][y] = 0;
-                        }
-                    } else {
-                        if (!alreadyCombined[X]) {
-                            gameBoard[X][y] *= 2;
-                            gameBoard[x][y] = 0;
-                            alreadyCombined[X] = true;
-                        } else {
-                            if (X - 1 != x) { // Check if shifting occurred
-                                gameBoard[X - 1][y] = value;
-                                gameBoard[x][y] = 0;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    public void pushLeft() {
-        System.out.println("Pushing left...");
-        for (int x = 0; x < 4; x++) {
-            boolean[] alreadyCombined = {false, false, false, false};
-            for (int y = 1; y < 4; y++) {
-                if (gameBoard[x][y] != 0) {
-                    int value = gameBoard[x][y];
-                    int Y = y - 1;
-                    while ((Y >= 0) && (gameBoard[x][Y] == 0)) {
-                        Y--;
-                    }
-                    if (Y == -1) {
-                        gameBoard[x][0] = value;
-                        gameBoard[x][y] = 0;
-                    } else if (gameBoard[x][Y] != value) {
-                        if (Y + 1 != y) { // Check if shifting occurred
-                            gameBoard[x][Y + 1] = value;
-                            gameBoard[x][y] = 0;
-                        }
-                    } else {
-                        if (!alreadyCombined[Y]) {
-                            gameBoard[x][Y] *= 2;
-                            gameBoard[x][y] = 0;
-                            alreadyCombined[Y] = true;
-                        } else {
-                            if (Y + 1 != y) { // Check if shifting occurred
-                                gameBoard[x][Y + 1] = value;
-                                gameBoard[x][y] = 0;
+                                board[X + 1][y] = value;
+                                board[x][y] = 0;
                             }
                         }
                     }
@@ -164,39 +94,172 @@ public class Game {
         }
     }
 
-    public void pushRight() {
-        System.out.println("Pushing right...");
-        for (int x = 0; x < 4; x++) {
+    public void pushDown(int[][] board) {
+        System.out.println("Pushing down...");
+        for (int y = 0; y < 4; y++) {
             boolean[] alreadyCombined = {false, false, false, false};
-            for (int y = 2; y > -1; y--) {
-                if (gameBoard[x][y] != 0) {
-                    int value = gameBoard[x][y];
-                    int Y = y + 1;
-                    while ((Y <= 3) && (gameBoard[x][Y] == 0)) {
-                        Y++;
+            for (int x = 2; x > -1; x--) {
+                if (board[x][y] != 0) {
+                    int value = board[x][y];
+                    int X = x + 1;
+                    while ((X <= 3) && (board[X][y] == 0)) {
+                        X++;
                     }
-                    if (Y == 4) {
-                        gameBoard[x][3] = value;
-                        gameBoard[x][y] = 0;
-                    } else if (gameBoard[x][Y] != value) {
-                        if (Y - 1 != y) { // Check if shifting occurred
-                            gameBoard[x][Y - 1] = value;
-                            gameBoard[x][y] = 0;
+                    if (X == 4) {
+                        board[3][y] = value;
+                        board[x][y] = 0;
+                    } else if (board[X][y] != value) {
+                        if (X - 1 != x) { // Check if shifting occurred
+                            board[X - 1][y] = value;
+                            board[x][y] = 0;
                         }
                     } else {
-                        if (!alreadyCombined[Y]) {
-                            gameBoard[x][Y] *= 2;
-                            gameBoard[x][y] = 0;
-                            alreadyCombined[Y] = true;
+                        if (!alreadyCombined[X]) {
+                            board[X][y] *= 2;
+                            board[x][y] = 0;
+                            alreadyCombined[X] = true;
                         } else {
-                            if (Y - 1 != y) { // Check if shifting occurred
-                                gameBoard[x][Y - 1] = value;
-                                gameBoard[x][y] = 0;
+                            if (X - 1 != x) { // Check if shifting occurred
+                                board[X - 1][y] = value;
+                                board[x][y] = 0;
                             }
                         }
                     }
                 }
             }
         }
+    }
+
+    public void pushLeft(int[][] board) {
+        System.out.println("Pushing left...");
+        for (int x = 0; x < 4; x++) {
+            boolean[] alreadyCombined = {false, false, false, false};
+            for (int y = 1; y < 4; y++) {
+                if (board[x][y] != 0) {
+                    int value = board[x][y];
+                    int Y = y - 1;
+                    while ((Y >= 0) && (board[x][Y] == 0)) {
+                        Y--;
+                    }
+                    if (Y == -1) {
+                        board[x][0] = value;
+                        board[x][y] = 0;
+                    } else if (board[x][Y] != value) {
+                        if (Y + 1 != y) { // Check if shifting occurred
+                            board[x][Y + 1] = value;
+                            board[x][y] = 0;
+                        }
+                    } else {
+                        if (!alreadyCombined[Y]) {
+                            board[x][Y] *= 2;
+                            board[x][y] = 0;
+                            alreadyCombined[Y] = true;
+                        } else {
+                            if (Y + 1 != y) { // Check if shifting occurred
+                                board[x][Y + 1] = value;
+                                board[x][y] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void pushRight(int[][] board) {
+        System.out.println("Pushing right...");
+        for (int x = 0; x < 4; x++) {
+            boolean[] alreadyCombined = {false, false, false, false};
+            for (int y = 2; y > -1; y--) {
+                if (board[x][y] != 0) {
+                    int value = board[x][y];
+                    int Y = y + 1;
+                    while ((Y <= 3) && (board[x][Y] == 0)) {
+                        Y++;
+                    }
+                    if (Y == 4) {
+                        board[x][3] = value;
+                        board[x][y] = 0;
+                    } else if (board[x][Y] != value) {
+                        if (Y - 1 != y) { // Check if shifting occurred
+                            board[x][Y - 1] = value;
+                            board[x][y] = 0;
+                        }
+                    } else {
+                        if (!alreadyCombined[Y]) {
+                            board[x][Y] *= 2;
+                            board[x][y] = 0;
+                            alreadyCombined[Y] = true;
+                        } else {
+                            if (Y - 1 != y) { // Check if shifting occurred
+                                board[x][Y - 1] = value;
+                                board[x][y] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean gameLost(){
+        int count = 0;
+        int difference = 0;
+        int[][] testBoard = new int[4][4];
+        boolean different = true;
+        for(int[] x: gameBoard){
+            for(int y: x){
+                if(y!=0){
+                    count++;
+                }
+            }
+        }
+        if(count==16){
+
+            for(int i=0; i<gameBoard.length;i++){
+                for (int j=0;j<gameBoard[i].length;j++){
+                    testBoard[i][j] = gameBoard[i][j];
+                }
+
+            }
+            pushUp(testBoard);
+            pushDown(testBoard);
+            pushLeft(testBoard);
+            pushRight(testBoard);
+        }
+        for(int i=0; i<gameBoard.length;i++){
+            for (int j=0;j<gameBoard[i].length;j++){
+                if(gameBoard[i][j]==testBoard[i][j]){
+                    difference++;
+                } else{
+                    difference = difference;
+
+                }
+            }
+        }
+        if(difference==16){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int[][] getBoard(){
+        return gameBoard;
+    }
+
+    public boolean gameWon(){
+        for(int i=0; i<gameBoard.length;i++){
+            for (int j=0;j<gameBoard[i].length;j++){
+                if(gameBoard[i][j]==2048){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public void clearBoard(){
+        gameBoard = new int[4][4];
     }
 }
